e966f9d1ce9a068fc42edf9373c56f295bc47bcf
Chris Flores <chris@Chriss-MacBook-Pro.local>
2019-07-29 13:52:26 -0700

    fix for account list and approval page

--- ProjectTemplate/ProjectServices.asmx.cs
+++ ProjectTemplate/ProjectServices.asmx.cs
@@ -22,12 +22,14 @@ namespace ProjectTemplate
 		private string dbID = "runtime";
 		private string dbPass = "!!Cis440";
 		private string dbName = "runtime";
-		////////////////////////////////////////////////////////////////////////
-		
-		////////////////////////////////////////////////////////////////////////
-		///call this method anywhere that you need the connection string!
-		////////////////////////////////////////////////////////////////////////
-		private string getConString() {
+
+
+        ////////////////////////////////////////////////////////////////////////
+
+        ////////////////////////////////////////////////////////////////////////
+        ///call this method anywhere that you need the connection string!
+        ////////////////////////////////////////////////////////////////////////
+        private string getConString() {
 			return "SERVER=107.180.1.16; PORT=3306; DATABASE=" + dbName+"; UID=" + dbID + "; PASSWORD=" + dbPass;
 		}
 		////////////////////////////////////////////////////////////////////////
@@ -200,34 +202,66 @@ namespace ProjectTemplate
             //our connection string comes from our web.config file like we talked about earlier
             string sqlConnectString = System.Configuration.ConfigurationManager.ConnectionStrings["myDB"].ConnectionString;
             //here's our query.  A basic select with nothing fancy.  Note the parameters that begin with @
-            string sqlSelect = "SELECT employeeId, adminFlag FROM Account WHERE employeeId=@idValue and AcctPassword=@passValue";
+            // string sqlSelect = "SELECT employeeId, adminFlag FROM Account WHERE employeeId=@idValue and AcctPassword=@passValue";
+            string sql = "SELECT * FROM AccountRequest WHERE EmployeeId=@idValue";
 
             //set up our connection object to be ready to use our connection string
             MySqlConnection sqlConnection = new MySqlConnection(sqlConnectString);
+            sqlConnection.Open();
+
+
             //set up our command object to use our connection, and our query
-            MySqlCommand sqlCommand = new MySqlCommand(sqlSelect, sqlConnection);
+            MySqlCommand sqlCommand = new MySqlCommand(sql, sqlConnection);
 
             //tell our command to replace the @parameters with real values
             //we decode them because they came to us via the web so they were encoded
             //for transmission (funky characters escaped, mostly)
             sqlCommand.Parameters.AddWithValue("@idValue", HttpUtility.UrlDecode(employeeId));
 
-            //a data adapter acts like a bridge between our command object and 
-            //the data we are trying to get back and put in a table object
-            MySqlDataAdapter sqlDa = new MySqlDataAdapter(sqlCommand);
-            //here's the table we want to fill with the results from our query
-            DataTable sqlDt = new DataTable();
-            //here we go filling it!
-            sqlDa.Fill(sqlDt);
-
-            pendingAccount.employeeId = sqlDt.Rows[0]["EmployeeId"].ToString();
-            pendingAccount.acctPassword = sqlDt.Rows[0]["AcctPassword"].ToString();
-            pendingAccount.firstName = sqlDt.Rows[0]["FirstName"].ToString();
-            pendingAccount.lastName = sqlDt.Rows[0]["LastName"].ToString();
-            pendingAccount.email = sqlDt.Rows[0]["Email"].ToString();
-            pendingAccount.requestDt = sqlDt.Rows[0]["RequestDt"].ToString();
+            try
+            {
+                MySqlDataReader dr;
+                dr = sqlCommand.ExecuteReader();
+                while (dr.Read())
+                {
+                    pendingAccount.employeeId = dr.GetString("EmployeeID");
+                    pendingAccount.acctPassword = dr.GetString("AcctPassword");
+                    pendingAccount.email = dr.GetString("Email");
+                    pendingAccount.firstName = dr.GetString("FirstName");
+                    pendingAccount.lastName = dr.GetString("LastName");
+                }
+                dr.Close();
+            }
+            catch (Exception)
+            {
+                
+            }
+            finally
+            {
+                if (sqlConnection.State == ConnectionState.Open)
+                {
+                    sqlConnection.Close();
+                }
+            }
 
             return pendingAccount;
+
+            ////a data adapter acts like a bridge between our command object and 
+            ////the data we are trying to get back and put in a table object
+            //MySqlDataAdapter sqlDa = new MySqlDataAdapter(sqlCommand);
+            ////here's the table we want to fill with the results from our query
+            //DataTable sqlDt = new DataTable("accountrequests");
+            ////here we go filling it!
+            //sqlDa.Fill(sqlDt);
+
+            //pendingAccount.employeeId = sqlDt.Rows[0]["EmployeeId"].ToString();
+            //pendingAccount.acctPassword = sqlDt.Rows[0]["pendingAccount.acctPassword"].ToString();
+            //pendingAccount.firstName = sqlDt.Rows[0]["FirstName"].ToString();
+            //pendingAccount.lastName = sqlDt.Rows[0]["LastName"].ToString();
+            //pendingAccount.email = sqlDt.Rows[0]["Email"].ToString();
+            //pendingAccount.requestDt = sqlDt.Rows[0]["RequestDt"].ToString();
+
+            // return pendingAccount;
         }
     }
 }
